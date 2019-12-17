fc77066fb44a7839e7733a2b90efc4392ce0f268
dileess <46401993+dileess@users.noreply.github.com>
2019-12-17 23:54:56 +0700

    Initial commit

---
+++ resources/views/layout/_nav.blade.php
@@ -0,0 +1,24 @@
+<!-- Navbar -->
+
+<nav class="navbar navbar-expand-lg bg-white navbar-dark bb-2 py-2" style="top:0px;">
+    <div class="container">
+        <div class="navbar-left">
+            <button class="navbar-toggler" type="button">&#9776;</button>
+            <a class="navbar-brand" href="#">
+                <img class="logo-dark" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/The_Boring_Company_Logo.svg/1200px-The_Boring_Company_Logo.svg.png" alt="logo">
+                <img class="logo-light" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/The_Boring_Company_Logo.svg/1200px-The_Boring_Company_Logo.svg.png" alt="logo">
+            </a>
+        </div>
+        <section class="navbar-mobile">
+            <nav class="nav nav-navbar ml-auto">
+                <a class="nav-link active" href="#">Home</a>
+                <a class="nav-link active" href="#">Campagin</a>
+              
+            </nav>
+            <button class="btn btn-primary btn-sm btn-round">Login</button>&nbsp;&nbsp;
+            <button class="btn btn btn-outline-success btn-sm btn-round">register</button>
+            
+        </section>
+    </div>
+</nav>
+<!-- /.navbar -->
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.master')
+
+@section('content_head')
+
+@endsection
+
+@section('content_footer')
+
+@endsection
+
+@section('content')
+
+ 
+@endsection
